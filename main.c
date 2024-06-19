eb9a32b717f693ba378b921869c61fba62ba5b93
ademnajjar48 <cctestingportugal@gmail.com>
2024-06-19 15:54:55 +0100

    Create addition function

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
-#include <stdiolib>
+#include <stdio.h>
+#include <stdlib.h>
 
-char User_command;
+char *User_command;
 void Help() {
-	printf("===Commands===");
-	printf("** a: addition");
-	printf("** s: subtraction **")
-	printf("** m: multiplication **");
-	printf("** d: division **");
-	printf("** r: squreroot **");
-	printf("** p: power **");
-	printf("** s: sine **");
-	printf("** c: cosine **");
-	printf("** t: tangent **");
-	printf("==============");
+	printf("===Commands===\n");
+	printf("** a: addition\n");
+	printf("** s: subtraction **\n");
+	printf("** m: multiplication **\n");
+	printf("** d: division **\n");
+	printf("** r: squreroot **\n");
+	printf("** p: power **\n");
+	printf("** s: sine **\n");
+	printf("** c: cosine **\n");
+	printf("** t: tangent **\n");
+	printf("==============\n");
 
 }
 void Greet() {
@@ -21,7 +22,27 @@ void Greet() {
 	Help();
 }
 void GetUserCommand() {
+	printf("(h): help\n");
+	printf("(e): exit\n");
+	printf("Enter a command: ");
 	scanf("%c" , User_command);
+}
+void addition() {
+	double* val1 = malloc(sizeof(double));
+	double* val2 = malloc(sizeof(double));
+	double* result = malloc(sizeof(double));
+	
+	printf("Enter first value: ");
+	scanf("%lf", val1);
+	printf("Enter second value: ");
+	scanf("%lf", val2);
+	
+	*result = *val1 + *val2;
+	printf("The result is: %lf \n", *result);
+	
+	free(val1);
+	free(val2);
+	free(result);
 }
 int main() {
 
