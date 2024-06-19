770795e1078bcf484c2b26d9091339fb183caafe
ademnajjar48 <cctestingportugal@gmail.com>
2024-06-19 09:29:59 +0100

    Initialize a greet function

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdiolib>
 
+void Help() {
+	printf("===Commands===");
+	printf("** a: addition");
+	printf("** s: subtraction **")
+	printf("** m: multiplication **");
+	printf("** d: division **");
+	printf("** r: squreroot **");
+	printf("** p: power **");
+	printf("** s: sine **");
+	printf("** c: cosine **");
+	printf("** t: tangent **");
+	printf("==============");
+
+}
 int main()
 {
 
